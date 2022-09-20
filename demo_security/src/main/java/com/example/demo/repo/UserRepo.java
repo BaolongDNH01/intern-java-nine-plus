0aa713a3881aafcc26a857e4730f2ab6aa20dd55
C0222G2-TranThiDiep <chenshiye1005@gmail.com>
2022-09-20 17:30:34 +0700

    up

--- demo_security/src/main/java/com/example/demo/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domain.User;
-
-public interface UserRepo extends JpaRepository<User,Long>{
-User findByUsername(String username);
-}
